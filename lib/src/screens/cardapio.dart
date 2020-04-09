87a8f8641fc11ee8f20307f341086e7e81769d7c
Carlos Nobrega <nobregacarlosjr@gmail.com>
2020-04-09 20:25:03 -0300

    adicao da tela de cardapio

--- lib/src/screens/cardapio.dart
+++ lib/src/screens/cardapio.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ifood_app/src/screens/add_food.dart';
 
 class Cardapio extends StatefulWidget {
   Cardapio();
@@ -11,51 +12,11 @@ class _CardapioState extends State<Cardapio> {
   @override
   void initState() {
     // bloc.getCardapio(idTurma: widget.idTurma);
-    // super.initState();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Color(0xfff5f5f5),
-        appBar: AppBar(
-          title: Text("Cardapio"),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ),
-        body: buildCardapio(context));
-  }
-
-  // Future getCardapio() {}
-
-  // Widget listStudents() {
-  //   return FutureBuilder(
-  //     future: getCardapio,
-  //     builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-  //       if (snapshot.hasData && !snapshot.hasError) {
-  //         return ListView.builder(
-  //           itemCount: snapshot.data?.length ?? 0,
-  //           itemBuilder: (BuildContext context, int index) {
-  //             if (snapshot.data.length == 0) {
-  //               return Center(
-  //                 child: Text("Nenhuma comida cadastrada"),
-  //               );
-  //             }
-  //             return buildCardapio(snapshot.data[index], context);
-  //           },
-  //         );
-  //       } else {
-  //         return Center(child: CircularProgressIndicator());
-  //       }
-  //     },
-  //   );
-  // }
-
-  Widget buildCardapio(BuildContext context) {
     return ListView.builder(
       itemCount: 4,
       itemBuilder: (BuildContext context, int index) {
@@ -63,7 +24,14 @@ class _CardapioState extends State<Cardapio> {
           child: ListTile(
             title: Text("Arroz"),
             subtitle: Text("Arroz com molho ...."),
-            onTap: null,
+            trailing: IconButton(icon: Icon(Icons.more_vert), onPressed: null),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          AddComida("Editar comida")));
+            },
           ),
         );
       },
